e1b1f37e159dbbbbe06d0401ae4f09b1971621de
MAwais <mawais54013@outlook.com>
2019-06-17 21:41:51 -0700

    added randomizer

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
 
@@ -27,6 +29,10 @@ class ViewController: UIViewController {
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         print(randomDiceIndex1)
+        
+        diceimageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceimageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
     
